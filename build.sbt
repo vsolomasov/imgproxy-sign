d6b66cee9ed619a86e41db6feaa8b37e33d96e25
Vyacheslav Solomasov <solomasov.vyacheslav@gmail.com>
2023-03-29 18:57:11 +0300

    Added informations

--- build.sbt
+++ build.sbt
@@ -1,4 +1,12 @@
 ThisBuild / scalaVersion := "2.13.10"
+
+ThisBuild / startYear := 2023.some
+ThisBuild / organization := "dev.vs"
+ThisBuild / licenses := List("Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0"))
+ThisBuild / developers := List(
+  Developer("vsolomasov", "Vyacheslav Solomasov", "solomasov.vyacheslav@gmail.com", url("https://github.com/vsolomasov"))
+)
+
 ThisBuild / tpolecatDevModeOptions += ScalacOptions.privateOption("macro-annotations")
 
 lazy val root = (project in file("."))
--- src/test/scala/dev/vs/imgproxy/ImgproxySignTest.scala
+++ src/test/scala/dev/vs/imgproxy/ImgproxySignTest.scala
@@ -7,8 +7,8 @@ object ImgproxySignTest extends SimpleIOSuite {
   private val secret = ImgproxySecret("39c57972d305c0dd1ddcabaa8c46ead7bfe1580691a11c16fa3e13c715a07885")
   private val salt = ImgproxySalt("31624205b3a51ae8dad082d9d503eb7832e578b49fc11d605768d44a98e52b4b")
   private val resizeValue = ImgproxyResizeValue("rs:fill:200:200:1/g:sm")
-  private val url = ImgproxyUrl("s3://wodaqota-stage/defaults/resource.svg")
-  private val expected = ImgproxySignedUrl("h3izg1ZhVqICHC2T_INaCGDFlvPjdzF9GHHTfpFauhQ/rs:fill:200:200:1/g:sm/czM6Ly93b2RhcW90YS1zdGFnZS9kZWZhdWx0cy9yZXNvdXJjZS5zdmc")
+  private val url = ImgproxyUrl("s3://bucket/defaults/resource.svg")
+  private val expected = ImgproxySignedUrl("hklUGiX13kriDkzcT6WK7nR5CcobB6bDN9WDRbejGHo/rs:fill:200:200:1/g:sm/czM6Ly9idWNrZXQvZGVmYXVsdHMvcmVzb3VyY2Uuc3Zn")
 
   test("Method sign should work correctly") {
     for {
